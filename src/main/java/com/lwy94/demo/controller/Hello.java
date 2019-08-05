94bd68f071f92c524fdcf2a9328c81dd2133d074
lwy2016 <1966941933@qq.com>
2019-08-05 11:41:22 +0800

    新增循环

--- src/main/java/com/lwy94/demo/controller/Hello.java
+++ src/main/java/com/lwy94/demo/controller/Hello.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 /**
  * @author liweiya
@@ -19,6 +20,18 @@ public class Hello {
         for (int i = 0; i < 100; i++) {
             System.out.println("love u " + i + "times");
         }
+
+        int[] arr = {1,43,455,2,42,5,12,2,41,4,14};
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length - 1; j++) {
+                if (arr[j] >= arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
         return "wooooooo" + hello;
     }
 }
